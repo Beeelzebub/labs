f6c12d7f54bee4ce7bedaf0af6bc506368238675
Beeelzebub <43516722+Beeelzebub@users.noreply.github.com>
2020-04-28 17:35:29 +0300

    new

---
+++ lab3/src/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    public static void main(String[] argv){
+        Transform transform = new Transform();
+        transform.setStr("Hi there! Welcome to the first task!");
+        transform.firtstTask("W","t", "k");
+        transform.setStr("My first phone number is 11-25-82 and second is 22-54-43");
+        transform.secondTask();
+    }
+
+}
---
+++ lab3/src/Transform.java
@@ -0,0 +1,49 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Transform {
+    String str;
+    int length;
+
+    public void firtstTask(String s, String first_s, String second_s){
+        stringOutput("\nFirst task:\n");
+        int from, to;
+        char[] temp = str.toCharArray();
+        from = str.indexOf(s);
+        Arrays.sort(temp, from, str.length());
+        System.out.println(temp);
+        from = str.indexOf(first_s);
+        to = str.lastIndexOf(second_s);
+        Arrays.sort(temp, from, to);
+        System.out.println(temp);
+    }
+
+    public void secondTask(){
+        stringOutput("\nSecond Task:\n");
+        StringBuilder strb = new StringBuilder(str);
+        for (int i = 0; i < length; i++){
+            if (strb.charAt(i) >= 48 && strb.charAt(i) <= 57 && i + 7 < length){
+                if (checkSubstring(strb.substring(i, (i + 8)))){
+                    if (strb.length() > length) strb.append(",");
+                    strb.append(" " + strb.substring(i, (i + 8)));
+                    i += 7;
+                }
+            }
+        }
+        System.out.println(strb);
+    }
+
+    private boolean checkSubstring(String substring){
+        if (substring.matches("\\d{2}(\\-)\\d{2}(\\-)\\d{2}")) return true;
+        else return false;
+    }
+
+    public void stringOutput(String s){
+        System.out.println(s + str);
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+        length = str.length();
+    }
+}
