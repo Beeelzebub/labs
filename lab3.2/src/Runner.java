e07e733c4cd99549295608373d832e3fda4b1c93
Beeelzebub <43516722+Beeelzebub@users.noreply.github.com>
2020-05-14 18:58:18 +0300

    rm

--- lab3.2/src/Runner.java
+++
@@ -1,9 +0,0 @@
-public class Runner {
-
-    public static void main(String[] argv){
-        Transform transform = new Transform();
-        transform.setStr("My first phone number is 11-25-82 and second is 22-54-43");
-        transform.secondTask();
-    }
-
-}
--- lab3.4/src/Sort.java
+++
@@ -1,5 +0,0 @@
-import java.util.Comparator;
-
-public interface Sort {
-    void sort(Object[] obj, Comparator comparator);
-}
